3dfeea639774e6bbaa4993d441225384fe53d12f
Spechtenhauser Florian <f.spechtenhauser@gmail.com>
2012-02-06 13:42:08 +0100

    initialized RTVs of both 3d textures in voronoi.cpp

--- Voronoi.cpp
+++ Voronoi.cpp
@@ -6,6 +6,16 @@ Voronoi::Voronoi(ID3D11Device *pd3dDevice, ID3D11DeviceContext *pd3dImmediateCon
 	m_pd3dDevice = pd3dDevice;
 	m_pd3dImmediateContext = pd3dImmediateContext;
 	m_pVoronoiEffect = pVoronoiEffect;
+
+	m_pDestColorTex3D = NULL;
+	m_pDestDistTex3D = NULL;
+	m_pDestColorTex3DRTV = NULL;
+	m_pDestDistTex3DRTV = NULL;
+	m_iTextureWidth = 0;
+	m_iTextureHeight = 0;
+	m_iTextureDepth = 0;
+	m_pSurface1 = NULL;
+	m_pSurface2 = NULL;
 }
 
 Voronoi::~Voronoi()
@@ -47,13 +57,46 @@ HRESULT Voronoi::Initialize()
 }
 
 HRESULT Voronoi::InitRendertargets()
-{}
+{
+	HRESULT hr;
+
+	assert(m_pDestColorTex3D != NULL);
+	assert(m_pDestDistTex3D != NULL);
+
+	D3D11_TEXTURE3D_DESC descColorTex3D;
+	m_pDestColorTex3D->GetDesc(&descColorTex3D);
+	D3D11_RENDER_TARGET_VIEW_DESC descCT3DRTV;
+	descCT3DRTV.Format = descColorTex3D.Format;
+	descCT3DRTV.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE3D;
+	descCT3DRTV.Texture3D.MipSlice = 0;
+	descCT3DRTV.Texture3D.FirstWSlice = 0;
+	descCT3DRTV.Texture3D.WSize = descColorTex3D.Depth;
+	V_RETURN(m_pd3dDevice->CreateRenderTargetView(m_pDestColorTex3D, &descCT3DRTV, &m_pDestColorTex3DRTV));
+
+	D3D11_TEXTURE3D_DESC descDistTex3D;
+	m_pDestDistTex3D->GetDesc(&descDistTex3D);
+	D3D11_RENDER_TARGET_VIEW_DESC descDT3DRTV;
+	descDT3DRTV.Format = descDistTex3D.Format;
+	descDT3DRTV.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE3D;
+	descDT3DRTV.Texture3D.MipSlice = 0;
+	descDT3DRTV.Texture3D.FirstWSlice = 0;
+	descDT3DRTV.Texture3D.WSize = descDistTex3D.Depth;
+	V_RETURN(m_pd3dDevice->CreateRenderTargetView(m_pDestDistTex3D, &descCT3DRTV, &m_pDestDistTex3DRTV));
+
+	return S_OK;
+}
 
 HRESULT Voronoi::InitShaders()
-{}
+{
+	//TODO
+}
 
 HRESULT Voronoi::RenderVoronoi(Surface *pSurface1, Surface *pSurface2, D3DXVECTOR3 vBBMin, D3DXVECTOR3 vBBMax)
-{}
+{
+	//TODO
+}
 
 void Voronoi::Draw()
-{}
+{
+	//TODO
+}
