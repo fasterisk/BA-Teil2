9515bf5173c85d1a71420fae6ea7405adb418642
Spechtenhauser Florian <f.spechtenhauser@gmail.com>
2012-04-22 21:48:20 +0200

    distance calculation in edgePS and vertexPS WORKS

--- Surface.cpp
+++ Surface.cpp
@@ -183,18 +183,20 @@ void Surface::RenderVoronoi(ID3DX11EffectTechnique* pTechnique)
 	UINT offset = 0;
 
 	//apply triangle technique & draw
-	m_pd3dImmediateContext->IASetVertexBuffers(0, 1, &m_pTriangleVertexBuffer, &stride, &offset);
-	m_pd3dImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-	pTechnique->GetPassByName("Triangle")->Apply( 0, m_pd3dImmediateContext);
-	m_pd3dImmediateContext->Draw(m_iNumTriangleVertices, 0);
+	//m_pd3dImmediateContext->IASetVertexBuffers(0, 1, &m_pTriangleVertexBuffer, &stride, &offset);
+	//m_pd3dImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	//pTechnique->GetPassByName("Triangle")->Apply( 0, m_pd3dImmediateContext);
+	//m_pd3dImmediateContext->Draw(m_iNumTriangleVertices, 0);
 	
 	//apply edge technique & draw
-	//m_pd3dImmediateContext->IASetVertexBuffers(0, 1, &m_pEdgeVertexBuffer, &stride, &offset);
-	//m_pd3dImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_LINELIST);
-	//pTechnique->GetPassByName("Edge")->Apply( 0, m_pd3dImmediateContext);
-	//m_pd3dImmediateContext->Draw(m_iNumEdgeVertices, 0);
+	m_pd3dImmediateContext->IASetVertexBuffers(0, 1, &m_pEdgeVertexBuffer, &stride, &offset);
+	m_pd3dImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_LINELIST);
+	pTechnique->GetPassByName("Edge")->Apply( 0, m_pd3dImmediateContext);
+	m_pd3dImmediateContext->Draw(m_iNumEdgeVertices, 0);
 
 	//apply point technique & draw
+	//m_pd3dImmediateContext->IASetVertexBuffers(0, 1, &m_pTriangleVertexBuffer, &stride, &offset);
+	//m_pd3dImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_POINTLIST);
 	//pTechnique->GetPassByName("Point")->Apply( 0, m_pd3dImmediateContext);
 	//m_pd3dImmediateContext->Draw(m_iNumTriangleVertices, 0);
 }
